fdd078b3a1c2ff246be01dcfc2880401371fee78
aynkz <aynkz@mail.ru>
2020-06-09 20:40:57 +0600

    Second commit

---
+++ Java0Tutor/1_Basics_of_software_code_development/by/epam/student/ippolitov/java/module_task3/E1c2.java
@@ -0,0 +1,28 @@
+package by.epam.student.ippolitov.java.module_task3;
+
+/* 2) Вычислите значение функции на отрезке [a,b] с шагом h */
+public class E1c2 {
+	public static void main(String args[]) {
+		double a;
+		double b;
+		double h;
+		double x;
+		a = -3;
+		b = 5;
+		h = 0.2; // шаг
+
+		if (a <= 2) {
+			for (x = a; x <= b; x = x + h) {
+				System.out.println(x + "   ");
+			}
+		}
+
+		if (a > 2) {
+			for (x = a; x <= b; x = x + h) {
+				System.out.println(x + "   ");
+			}
+		}
+
+	}
+
+}
--- Java0Tutor/1_Basics_of_software_code_development/by/epam/student/ippolitov/java/module_task3/T2.java
+++
@@ -1,5 +0,0 @@
-package by.epam.student.ippolitov.java.module_task3;
-/* 2) Вычислите значение функции на отрезке [a,b] с шагом h */
-public class T2 {
-
-}
